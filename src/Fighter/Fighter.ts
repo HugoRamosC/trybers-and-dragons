72958bd09ab849a739de91da97f3a1dde709977f
HugoRamosC <hugor.castilho@gmail.com>
2023-03-24 23:46:01 -0300

    feat: simplefighter interface - req08

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,12 +1,13 @@
 import IEnergy from '../Energy';
+import ISimpleFighter from './SimpleFighter';
 
-export default interface IFighter {
+export default interface IFighter extends ISimpleFighter {
   lifePoints: number;
   strength: number;
   defense: number;
   energy?: IEnergy;
-  attack(enemy: IFighter): void;
+  // attack(enemy: IFighter): void;
   special?(enemy: IFighter): void;
   levelUp(): void;
-  receiveDamage(attackPoints: number): number
+  // receiveDamage(attackPoints: number): number
 }
---
+++ src/Fighter/SimpleFighter.ts
@@ -0,0 +1,8 @@
+export default interface ISimpleFighter {
+  lifePoints: number;
+  strength: number;
+
+  attack(enemy: ISimpleFighter): void
+
+  receiveDamage(attackPoints: number): number
+}

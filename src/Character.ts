b8609038036044e230ce9868bd2c876f8f8b514d
HugoRamosC <hugor.castilho@gmail.com>
2023-03-27 21:33:15 -0300

    fix: character class levelup function

--- src/Character.ts
+++ src/Character.ts
@@ -90,12 +90,12 @@ export default class Character implements IFighter {
     this._strength += getRandomInt(1, 10);
     this._dexterity += getRandomInt(1, 10);
     this._defense += getRandomInt(1, 10);
-    // this._lifePoints += getRandomInt(1, 10);
+    this._lifePoints += getRandomInt(1, 10);
     this._energy.amount = 10;
-    if (this._maxLifePoints > this._race.maxLifePoints) {
-      this._maxLifePoints = this._race.maxLifePoints;
+    if (this._lifePoints > this._race.maxLifePoints) {
+      this._lifePoints = this._race.maxLifePoints;
+      // this._lifePoints = this._race.maxLifePoints;
     }
-    this._lifePoints = this._race.maxLifePoints;
     this._level += 1;
   }
 }
